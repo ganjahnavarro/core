6dfd2ba2c995809602a9ca5ce91afc95c0fb27d0
ganjahnavarro <ganjahnavarro@gmail.com>
2018-08-29 22:33:52 +0800

    updated config mapper date formatting

--- src/main/java/core/dto/mapper/ConfigMapper.java
+++ src/main/java/core/dto/mapper/ConfigMapper.java
@@ -3,7 +3,9 @@ package core.dto.mapper;
 import java.util.List;
 
 import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import core.dto.ConfigData;
@@ -14,8 +16,10 @@ public interface ConfigMapper {
 	
 	ConfigMapper INSTANCE = Mappers.getMapper(ConfigMapper.class);
 
+	@Mapping(target = "modifiedDate", source = "modifiedDate", dateFormat = "MM/dd/yyyy HH:mm")
 	ConfigData toData(Config config);
 	
+	@IterableMapping(dateFormat = "MM/dd/yyyy HH:mm")
 	List<ConfigData> toData(List<Config> configs);
 	
 	@InheritInverseConfiguration
